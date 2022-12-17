92d1f87ed206e55dde3fdbe68ee384fb0a503d39
Diane Guzman <117790100+dianeooty@users.noreply.github.com>
2022-12-17 10:12:05 -0800

    updates

--- Pybank/main.py
+++ Pybank/main.py
@@ -24,42 +24,39 @@ with open(budget_csv, "r", encoding="utf8") as csvfile:
     #Creating a duplicate list of profit/loss for change in profit calculation
     for line in csvreader:
         dates.append(line[0])
-        p1.append(line[1])
+        p1.append(line[1])   
         p2.append(line[1])
 
-    #Zipped the two lists and converted to a dictionary
-    newlist = dict(zip(dates, p1))
-
     #Converting the values in pl from strings to integers and summing up for total
-    i_p1 = [int(x) for x in p1]
-    i_p2 = [int(x) for x in p1]
-    total = (sum(i_p1)) 
-
+    p1 = [int(x) for x in p1]
+    total = (sum(p1)) 
+    
     #Popping index 0 on duplicate list to use to subtract for changes
-    i_p2.pop(0)
+    p2 = [int(x) for x in p1]
+    p2.pop(0)
     
     #Zipping the two lists of profits/losses to create a new lists with the calculated changes in profit
-    for x, y in zip(i_p1, i_p2):
+    for x, y in zip(p1, p2):
         c = x - y
         change.append(c)
     
-    #Calculating the average of change in profits
-    average = round(sum(change) / len(i_p2),2)
-        
-    #Finding the max and min from the change list  
+    #Calculating the average of the changes in profit using sum, rounding to 2 decimals and dividing by list length
+    average = round(sum(change) / len(p2), 2)
+    
+    #Finding the max and min from the change list using min and max 
     increase = (max(change))
-    decrease = (min(change))
-
-    #Adding index 0 back into the list for $0 change at the beginning of the year
-    change.insert(0,0)  
-     
-    #Zipping the lists and converting to dictionary to get key-value pairs for the matching datas for increase/decrease
-    d = dict(zip(dates, change)) 
+    decrease = (min(change)) 
+    
+    #Adding index 0 back into the list for $0 change at the beginning 
+    change.insert(0,0) 
+    
+    #Zipping the lists and converting to dictionary
+    d = dict(zip(dates, change))
     
     #Using the values of increase and decrease to find the dictionary keys for increase/decrease dates
     date = [k for k, v in d.items() if v == increase]
     date2 = [k for k, v in d.items() if v == decrease]
-
+    
     #Printing Finanacial Analysis Results
     print("------------------------------------------------------------")
     print("Financial Analysis")
@@ -70,7 +67,7 @@ with open(budget_csv, "r", encoding="utf8") as csvfile:
     print(f"Greatest Increase in Profits:  {date} ${increase}")
     print(f"Greatest Decrease in Profits:  {date2} ${decrease}")
     print("------------------------------------------------------------")
-
+    
 #Creating a variable called analysis as a list of the lines to write to txt file
 analysis = ["-------------------------------------------------------",
             "",
@@ -80,6 +77,8 @@ analysis = ["-------------------------------------------------------",
             "",
             "Total Months: 86",
             "",
+            "Total:  $22564198",
+            "",
             "Average Change:  $8311.11",
             "",
             "Greatest Increase in Profits:  Feb-14  $1825558",
@@ -87,6 +86,7 @@ analysis = ["-------------------------------------------------------",
             "Greatest Decrease in Profits:  Aug-16  $-1862002",
             "",
             "-------------------------------------------------------"]
+
 #Naming the txt file
 filename = "financialanalysis.txt"
 

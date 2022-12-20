efab66c7d54a8cad6083cf4630b95df0087e429a
Diane Guzman <117790100+dianeooty@users.noreply.github.com>
2022-12-19 23:52:28 -0800

    upload

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,7 +3,7 @@ import os
 import csv
 
 #Filepath for csv
-election_csv = os.path.join("PyPoll","resources","election_data.csv")
+election_csv = os.path.join("python-challenge","PyPoll","resources","election_data.csv")
 
 #Creating empty lists, dictionary and variables
 candidate_list = []
